a7ebec92f2eefc1014d713d3ecbf90bb8d8b2d1f
Kartikeyyyy <87395168+Kartikeyyyy@users.noreply.github.com>
2021-07-13 17:09:43 -0400

    Create projectfile.py

---
+++ projectfile.py
@@ -0,0 +1,16 @@
+import csv
+import json
+import pandas as pd
+from pandas.io.json import json_normalize
+
+def json_csv():
+
+    with open('my_json_file.JSON') as data_file:
+        data=json.load(data_file)
+    normalized_df = pd.json_normalize(data)
+    normalized_df.to_csv('my_csv_file.csv',index=False)
+    return
+
+
+
+json_csv()
